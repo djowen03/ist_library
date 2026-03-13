f453c2a260f03d7891899264c1e0c419a6e27822
djowen03 <dominicus.jovian@gmail.com>
2026-03-13 16:40:27 +0700

    initial commit

---
+++ src/test/java/com/example/ist_library/IstLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ist_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IstLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
